dcaa44509599e1591f2099a59aab1b926a05e91e
Lukasz Czerniak <Lukasz.Czerniak@pl.ey.com>
2019-01-13 14:40:58 +0100

    show temp on scree

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
 
 var app = express();
 const port=process.env.PORT || 3000
@@ -7,6 +9,14 @@ const port=process.env.PORT || 3000
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+const bodyParser = require('body-parser')
+
+const middleware = [
+  bodyParser.urlencoded()
+]
+app.use(middleware)
+
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
@@ -31,6 +41,38 @@ app.get('/weather-forecast', (req, res) => {
         currentYear: new Date().getFullYear() 
      });
 });
+
+app.post('/weather-forecast', (req, res) => {
+    var data = req.body;
+
+
+    geocode.geocodeAddress(data.city, (errorMessage, results) => {
+        if (errorMessage) {
+            console.log(errorMessage);
+        } else {
+            console.log(results.address);
+            weather.getWeather(results.lat, results.lng, (errorMessage, results) => {
+                if (errorMessage) {
+                    console.log(errorMessage);
+                } else {
+                    console.log(JSON.stringify(results, undefined, 2));
+                } 
+                res.render('forecast-weather.hbs', {
+                    pageTitle: 'Weather Forecast',
+                    welcomeMessage: 'Check the weather!',
+                    currentYear: new Date().getFullYear(),
+                    city: data.city, // { message, email }
+                    temp1: results.temp1,
+                    temp2: results.temp2
+            
+                })
+
+            })}
+        }
+    )
+
+
+  })
 
 app.listen(port, () => {
     console.log("Server is on port 3000.");
--- weather/weather.js
+++ weather/weather.js
@@ -4,15 +4,32 @@ const geocode = require('../geocode/geocode');
 
 geocode.geocodeAdress
 
-var getWeather = (lat, lng) => {
+var getWeather = (lat, lng, callback) => {
+
+    var res;
+
     request({
     url: `https://api.darksky.net/forecast/de50c04feda65b3dc4360c189b43a572/${lat},${lng}?&units=si`,
     json: true
     }, (error, response, body) => {
+
+        console.log(`Actual temperature is: ${body.currently.temperature}. It feels like: ${body.currently.apparentTemperature}.`);
+    
         if (error) {
             callback("Unable to connect to Google servers.")
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback("Unable to find that address.");
+        } else {
+        // console.log(JSON.stringify(body, undefined, 2));
+        callback(undefined, {
+            temp1: body.currently.temperature,
+            temp2: body.currently.apparentTemperature
+        });
+
         }
-        console.log(`Actual temperature is: ${body.currently.temperature}. It feels like: ${body.currently.apparentTemperature}.`);
+    
     });
+
+    
 }
 module.exports.getWeather = getWeather;
