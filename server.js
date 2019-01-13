000e9e7523b7b22438b8a02e11973cb80a0f2c1b
Lukasz Czerniak <Lukasz.Czerniak@pl.ey.com>
2019-01-13 13:07:31 +0100

    add hbs

--- server.js
+++ server.js
@@ -4,19 +4,32 @@ const hbs = require('hbs');
 var app = express();
 const port=process.env.PORT || 3000
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
        pageTitle: 'Home Page',
-       welcomeMessage: 'Welcome to the franQyy-weather App!',
+       welcomeMessage: 'Welcome to the FranQyy-weather App!',
        currentYear: new Date().getFullYear() 
     });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage: 'It is an awesome app. Thanks to visit me!',
+        currentYear: new Date().getFullYear() 
+     });
+});
+
+app.get('/weather-forecast', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'Weather Forecast',
+        welcomeMessage: 'Check the weather!',
+        currentYear: new Date().getFullYear() 
+     });
 });
 
 app.listen(port, () => {
