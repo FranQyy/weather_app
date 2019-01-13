80f4bc3ef38bfb29a40d30ba4fa010e884e8b48b
Lukasz Czerniak <Lukasz.Czerniak@pl.ey.com>
2019-01-13 12:37:31 +0100

    Azure config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
+const port=process.env.PORT || 3000
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -14,6 +15,6 @@ app.get('/about', (req, res) => {
     res.render('about.hbs');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is on port 3000.");
 });
