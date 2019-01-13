f33a3dd6d4d1c39bca8fe7b73a29de8ee07b590f
Lukasz Czerniak <Lukasz.Czerniak@pl.ey.com>
2019-01-13 12:58:52 +0100

    adds advanced templates

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello Express!');
+    res.render('home.hbs', {
+       pageTitle: 'Home Page',
+       welcomeMessage: 'Welcome to the franQyy-weather App!',
+       currentYear: new Date().getFullYear() 
+    });
 });
 
 app.get('/about', (req, res) => {
